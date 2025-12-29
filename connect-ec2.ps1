8a0c5aeca6d9e6e0083f43d47b3b08abd5060f18
Yao Amevi A. Sossou <47247401+soyames@users.noreply.github.com>
2025-12-29 23:58:35 +0100

    update

--- connect-ec2.ps1
+++ connect-ec2.ps1
@@ -2,11 +2,11 @@
 # Save this as: connect-ec2.ps1
 
 $keyPath = "C:\Users\soyam\.ssh\bintacura_ec2"
-$server = "ec2-user@13.53.170.59"
+$server = "ec2-user@16.171.180.104"
 
 Write-Host "Connecting to BintaCura EC2 Server..." -ForegroundColor Cyan
 Write-Host "Server: $server" -ForegroundColor White
-Write-Host "Instance: i-0b89178e5348db0a4" -ForegroundColor White
+Write-Host "Instance: i-074ee5b498fde1faa (bintacura-web-server-sg3)" -ForegroundColor White
 Write-Host "Key: $keyPath" -ForegroundColor White
 Write-Host ""
 
